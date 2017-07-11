ac25fd84610bdf83d1ef2270e0649915ba1c6ec8
Andrew Parks and Michael Woldemedihin <andrew@>
2017-07-11 14:36:24 -0700

    create click function to insert replace a vowel with a -

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
-  $("#formSubmit").click(function(event) {
-    debugger;
-    var countTo = parseInt($("#countTo").val());
-    var countBy = parseInt($("#countBy").val());
+  // $("#formSubmit").click(function(event) {
+  //   debugger;
+  //   var countTo = parseInt($("#countTo").val());
+  //   var countBy = parseInt($("#countBy").val());
+  //
+  //   for(var i = countBy; i <= countTo; i += countBy){
+  //     $("#youCountedNumber").append(i + "<br>");
+  //   }
+  //   event.preventDefault();
+  // });
 
-    for(var i = countBy; i <= countTo; i += countBy){
-      $("#youCountedNumber").append(i + "<br>");
+  $("#btnPuzzle").click(function (event) {
+    var sentence = $("#sentence").val();
+    var puzzle = [];
+    for(var i = 0; i < sentence.length; i++){
+      if(sentence[i] === "a" ||sentence[i] === "e"||sentence[i] === "i"||sentence[i] === "o"||sentence[i] === "u") {
+         puzzle.push("-");
+       }
+      else{
+        puzzle.push(sentence[i])
+      }
     }
+    puzzle.toString();
+    $("#puzzleOutput").append(puzzle);
     event.preventDefault();
-
-
-
   });
-
 });
